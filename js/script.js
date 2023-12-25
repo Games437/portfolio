7a4c2b204b0f1d23dcd424f28e767536e4ae1482
Games437 <thanathip437@gmail.com>
2023-12-25 11:35:32 +0700

    V.2.1

---
+++ js/script.js
@@ -0,0 +1,46 @@
+function scrollToElement(elementSelector, instance = 0) {
+    // Select all elements that match the given selector
+    const elements = document.querySelectorAll(elementSelector);
+    // Check if there are elements matching the selector and if the requested instance exists
+    if (elements.length > instance) {
+        // Scroll to the specified instance of the element
+        elements[instance].scrollIntoView({ behavior: 'smooth' });
+    }
+}
+
+const link1 = document.getElementById("link1");
+const link2 = document.getElementById("link2");
+const link3 = document.getElementById("link3");
+const link4 = document.getElementById("link4");
+
+
+
+link1.addEventListener('click', () => {
+    scrollToElement('.about');
+});
+
+link2.addEventListener('click', () => {
+    scrollToElement('.certificate');
+});
+
+link3.addEventListener('click', () => {
+    scrollToElement('.project');
+});
+
+link4.addEventListener('click', () => {
+    scrollToElement('.column');
+});
+
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname){
+    for(tablink of tablinks){
+        tablink.classList.remove("active-link");
+    }
+    for(tabcontent of tabcontents){
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
